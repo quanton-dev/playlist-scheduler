65819c87798500587e211bac2e45dccd1847ab39
quanton-dev <yusufgndz99@gmail.com>
2022-11-20 01:15:40 +0300

    init project

---
+++ src/main/java/com/quantondev/playlistscheduler/PlaylistSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.quantondev.playlistscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantondev/playlistscheduler/PlaylistSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantondev.playlistscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
